db784d9a0ca0517c3df35b5fd4878c3ae2bad2e5
uforgot <uforgot@designfever.com>
2017-07-19 13:13:33 +0900

    site source created

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const config = {
+    context: path.resolve(__dirname, './site/assets_src/'),
+    entry: {
+        'app':['./js/app.js']
+    },
+
+    output: {
+        path: path.resolve(__dirname, './site/assets/'),
+        filename: 'js/[name].bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use:[{
+                    loader: 'vue-loader'
+                }]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [{
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['es2015', {modules: false}]
+                            ]
+                        }
+                    }]
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+
+    plugins: [
+        new CopyWebpackPlugin(
+            [
+                {from:"./js/vendor/modernizr.js", to:"./js/vendor/modernizr.js"},
+                {from:"./js/vendor/detectizr.js", to:"./js/vendor/detectizr.js"},
+
+
+                {from:"./css/**/*"},
+                {from:"./fonts/**/*"},
+                {from:"./images/**/*"},
+                {from:"./video/**/*"},
+                {from:"./data/**/*"}
+            ]
+        )
+    ]
+};
+
+module.exports = config;
+
+
+//https://github.com/vuejs-templates/webpack-simple/tree/master/template
